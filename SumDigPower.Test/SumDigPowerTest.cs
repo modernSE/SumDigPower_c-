a8f1fe4bf4db64a732600874cbcf2f678f310e97
Tanja.Binder <tanja.binder@yellowmap.de>
2017-06-22 10:27:37 +0200

    inital commit

---
+++ SumDigPower.Test/SumDigPowerTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumDigPower.Test
+{
+    [TestClass]
+    public class SumDigPowerTest
+    {
+        private SumDigPower SumDigPower;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            SumDigPower = new SumDigPower();
+        }
+
+        [TestMethod]
+        public void TenTo150_contains89and135()
+        {
+            CollectionAssert.AreEqual(SumDigPower.sumDigPow(10, 150), new List<long>(){ 89L, 135L});
+        }
+
+        [TestMethod]
+        public void ZeroTo100_firstTenNumbersAnd89ShoudBeContained()
+        {
+            CollectionAssert.AreEqual(SumDigPower.sumDigPow(1, 100), new List<long>() { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 89L});
+        }
+
+        [TestMethod]
+	    public void ninetyTo100_isEmpty()
+        {
+            CollectionAssert.AreEqual(SumDigPower.sumDigPow(90, 100), new List<long>());
+        }
+
+
+	    [TestMethod]
+	    public void TenTo100_only89Contained() {
+		    CollectionAssert.Contains(SumDigPower.sumDigPow(10, 100), 89L);
+	    }
+
+        [TestMethod]
+	    public void firstTenNumbers_shouldBeAllContained() {
+		    CollectionAssert.AreEqual(SumDigPower.sumDigPow(1, 10), new List<long>() { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L});
+	    }
+
+    }
+}
---
+++ SumDigPower/SumDigPower.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumDigPower
+{
+    public class SumDigPower
+    {
+        public List<long> sumDigPow(long a, long b)
+        {
+
+            List<long> result = new List<long>();
+
+            for (long i = a; i < b; i++)
+            {
+
+                List<long> longs = new List<long>();
+
+                String temp = i.ToString();
+
+                // Split values
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    longs.Add(Convert.ToInt64(temp[j].ToString()));
+                }
+
+                // Create sum
+                long sum = 0;
+                for (int j = 1; j <= longs.Count(); j++)
+                {
+                    sum += (long)Math.Pow(longs[j - 1], j);
+                }
+
+                // Test if sum is equal
+                if (sum == i)
+                {
+                    result.Add(sum);
+                }
+
+            }
+
+            return result;
+
+        }
+    }
+}
